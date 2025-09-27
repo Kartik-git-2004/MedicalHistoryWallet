c8f3ec07f375b5e6fc4b163b39bb736b3dc22c60
Kartik-git-2004 <kartiksharmapmsss@gmail.com>
2025-09-27 17:44:49 +0000

    Medicalhistory class

--- src/Medicalhistory.java
+++ src/Medicalhistory.java
@@ -1,2 +1,33 @@
 public class Medicalhistory {
+    private String date;
+    private String condition;
+    private String medicines;
+    private String note;
+
+
+    public Medicalhistory(String date , String condition , String medicines,String note){
+        this.date = date;
+        this.condition = condition;
+        this.medicines = medicines;
+        this.note= note;
+    }
+
+    public String getDate(){
+        return date;
+    }
+    public String getCondition(){
+        return condition;
+    }
+    public String getMedicines(){
+        return medicines;
+    }
+    public String getNote(){
+        return note;
+    }
+
+    public String toString(){
+        return "[ "+date+" ]"+"[ condition : "+condition+" ]\n"
+                 +"[ Medication -> "+medicines+" ]"+"[ Docotor Note --> "+note+" ]";
+    }
+
 }
